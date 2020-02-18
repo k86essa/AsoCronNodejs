81aabdd33673b1e1463b1a932bf7d10c32ef2802
nperdomo <nerioperdomo28@gmail.com>
2020-02-18 11:24:01 -0400

    agregados mensajes de debug

--- respaldos/archivos_git.js
+++ respaldos/archivos_git.js
@@ -2,12 +2,16 @@ const shellExec =require('shell-exec');
 async function backupArc(ruta, rama)
 {
     let resp = await shellExec(`cd ${ruta} && git branch`);
+    console.log(`cd ${ruta} && git branch:`+resp.stdout);
     if(!resp.stdout.includes(`* ${rama}`))
     {
+        console.log(`* ${rama}:`+resp.stdout);
         resp = await shellExec(`cd ${ruta} && git checkout ${rama}`);
+        console.log(`cd ${ruta} && git checkout ${rama}:`+resp.stdout);
         if(!resp.stderr.includes(`Switched to branch '${rama}'`)){console.error('error chk'); return false;}
     }
     resp = await shellExec(`cd ${ruta} && git status`);
+    console.log(`cd ${ruta} && git status:`+resp.stdout);
     if(!resp.stdout.includes('Untracked files:')){console.log('Sin archivos pendientes'); return true;}
     resp = await shellExec(`cd ${ruta} && git add . && git commit -m "Respaldo" && git status`);
     if(!resp.stdout.includes('working tree clean')){console.error('error revisar');return false;}
